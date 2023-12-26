05a5434dea1ead71ba278bed87967daacbdab020
Arpita Barode <100395290+ArpitaBarode@users.noreply.github.com>
2023-12-25 22:12:01 -0800

    Add files via upload

---
+++ TempretureConversion.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class TempretureConversion {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENter tempreature in celcius");
+        float tempc=sc.nextFloat();
+        float tempF=(tempc * 9/5)+32;
+        System.out.println(tempF);
+
+        
+    }
+    
+}
